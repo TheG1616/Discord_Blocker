99dbef40d88c9b28f5c69ecbed79fe270c24753f
yaligabay1616 <yaligabay717@gmail.com>
2026-09-15 15:09:13 +0300

    add text_analyzer

--- check_main.py
+++ check_main.py
@@ -4,7 +4,7 @@ import text_moudle as tx
 
 
 def main():
-    string_1 = "yali, gabay. hello,// world$ niger knife ## "
+    string_1 = "shit ass"
     string_1 = tx.remove_punct(string_1).strip()
     print(string_1)
     if tx.if_severe_sentence(tx.text_analyzer(string_1)):
