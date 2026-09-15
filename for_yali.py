e1fdea9dc137e24605e5e27257a0efa883a78229
yaligabay1616 <yaligabay717@gmail.com>
2026-09-15 13:59:43 +0300

    add text_analyzer

--- for_yali.py
+++ for_yali.py
@@ -1,5 +1,5 @@
 import text_moudle
 
-string_1 = "yali, gabay. hello,// world$ niger knife"
-
-print(text_moudle.text_analyzer(text_moudle.remove_punct(string_1)))
+string_1 = "yali, gabay. hello,// world$ niger knife ## "
+print(text_moudle.remove_punct(string_1))
+print(text_moudle.if_severe_sentence(text_moudle.text_analyzer(text_moudle.remove_punct(string_1))))
--- text_moudle.py
+++ text_moudle.py
@@ -10,8 +10,6 @@ def make_list_of_bad_words(data):
     return list_of_bad_words
 
 
-
-
 def remove_punct(text):
     new_words = ""
     for word in text:
@@ -23,12 +21,16 @@ def remove_punct(text):
 
 def text_analyzer(string):
     list_of_bad_words = make_list_of_bad_words(read_file("bad_words"))
-    list_sentence = string.split(" ")
+    list_sentence = string.strip().split(" ")
     count_bad_words = 0
     for word in list_sentence:
         if word in list_of_bad_words:
             count_bad_words+=1
+    print(list_sentence)
     return count_bad_words / len(list_sentence)
+
+def if_severe_sentence(float_num):
+    return float_num > 0.2
 
 
 print(make_list_of_bad_words(read_file("bad_words")))
