2aa889183540be5a5efcb9e34df0be2d816b4751
Yarin-Dev <117749540+Yarin-Dev@users.noreply.github.com>
2026-09-16 13:24:11 +0300

    Implement user kick, ban, and timeout for offenses

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ from nextcord.ext import commands, tasks
 from constants import TOKEN, PREFIX
 from text_moudle import remove_punct, text_analyzer, if_severe_sentence
 from image import check_image
+from care_json import add_new_event, get_settings, get_count_of_offence
 
 # intents
 intents = nextcord.Intents.default()
@@ -34,7 +35,6 @@ async def on_message(message):
     # bot won't reply himself in a endless loop.
     if message.author.bot:
         return
-
     content = remove_punct(message.content)
     percent = text_analyzer(content)
 
@@ -43,14 +43,25 @@ async def on_message(message):
             return
 
         else:
-            try:
-                duration = nextcord.utils.utcnow() + timedelta(minutes=1)
+            settings = get_settings()
+            amount = get_count_of_offence(message.author.name) + 1
+            if amount == settings["kick_amount"]:
+                await message.author.kick(reason="offensive image")
+                await message.channel.send(f"**{message.author.mention} has been kicked for offensive image.**")
+                add_new_event(message.author, str(message.created_at), "image", "kick")
+
+            elif amount == settings["ban_amount"]:
+                await message.author.ban(reason="offensive image")
+                await message.channel.send(f"**{message.author.mention} has been banned for offensive image.**")
+                add_new_event(message.author, str(message.created_at), "image", "ban")
+
+            else:
+                duration = nextcord.utils.utcnow() + timedelta(minutes=settings["timeout_duration"])
                 await message.author.edit(timeout=duration, reason="offensive image")
                 await message.channel.send(f"**{message.author.mention} has been timed out for offensive image.**")
-                await message.delete()
+                add_new_event(message.author, str(message.created_at), "image", "timeout")
 
-            except:
-                print("I don't have permissions to time out the user.")
+            await message.delete()
 
 
     # considered offensive
@@ -59,15 +70,27 @@ async def on_message(message):
 
         # Timeout user
         if response == "offensive":
-            duration = nextcord.utils.utcnow() + timedelta(minutes=1)
-            try:
-                await message.author.edit(timeout=duration, reason="offensive words")
-                await message.channel.send(f"**{message.author.mention} has been timed out for offensive words.**")
-                await message.delete()
+            settings = get_settings()
+            amount = get_count_of_offence(message.author.name) + 1
+            if amount == settings["kick_amount"]:
+                await message.author.kick(reason="offensive text")
+                await message.channel.send(f"**{message.author.mention} has been kicked for offensive text.**")
+                add_new_event(message.author, str(message.created_at), "text", "kick")
 
-            except:
-                print("I don't have permissions to time out the user.")
+            elif amount == settings["ban_amount"]:
+                await message.author.ban(reason="offensive text")
+                await message.channel.send(f"**{message.author.mention} has been banned for offensive text.**")
+                add_new_event(message.author, str(message.created_at), "text", "ban")
+
+            else:
+                duration = nextcord.utils.utcnow() + timedelta(minutes=settings["timeout_duration"])
+                await message.author.edit(timeout=duration, reason="offensive text")
+                await message.channel.send(f"**{message.author.mention} has been timed out for offensive text.**")
+                add_new_event(message.author, str(message.created_at), "text", "timeout")
+
+            await message.delete()
 
     await bot.process_commands(message)
+
 
 bot.run(TOKEN)
